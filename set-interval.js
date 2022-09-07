6d58490886113397468b1e1a4a1b7bf0c873304d
Rocko <codingwithrock@gmail.com>
2022-09-07 18:00:47 +0600

    done

--- set-interval.js
+++ set-interval.js
@@ -1,14 +1,19 @@
 increment = 0;
+
+
 console.log("working: " + increment);
+// !Stored Value For ID.
 const interValID = setInterval(() => {
     console.log(increment++);
+    // !nested interVal.
     setTimeout(() => {
         console.log("working: " + increment);
 
+        // !Fun With SetTimeOut.
         if (increment >= 5) {
             clearInterval(interValID);
             console.log(increment++);
-            
+
         }
 
     }, 1000)
