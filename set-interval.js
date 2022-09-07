073d10047d9b1906bd5b23292f32b072f5dcfc52
Rocko <codingwithrock@gmail.com>
2022-09-07 18:03:29 +0600

    done

--- set-interval.js
+++ set-interval.js
@@ -1,20 +1,44 @@
 increment = 0;
 
+synchronous = async()=> {
+   
+    try{
+        
+        // !Stored Value For ID.
+        
+        // Checking Await... by fetch...
 
-console.log("working: " + increment);
-// !Stored Value For ID.
-const interValID = setInterval(() => {
-    console.log(increment++);
-    // !nested interVal.
-    setTimeout(() => {
-        console.log("working: " + increment);
 
-        // !Fun With SetTimeOut.
-        if (increment >= 5) {
-            clearInterval(interValID);
+        // ! We don't need wait so no wait... git push
+        const interValID = setInterval(() => {
             console.log(increment++);
+            // !nested interVal.
+            setTimeout(() => {
+                console.log("working: " + increment);
+        
+                // !Fun With SetTimeOut.
+                if (increment >= 5) {
+                    clearInterval(interValID);
+                    console.log(increment++);
+        
+                }
+        
+            }, 1000)
+        }, 1000)
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
 
-        }
+    finally
+    {
 
-    }, 1000)
-}, 1000)
+        console.log("Our Mission is DONE!")
+        console.log("working: " + increment);
+        // data =  response.json();
+        // console.log(data);
+    }
+}
+
+synchronous();
